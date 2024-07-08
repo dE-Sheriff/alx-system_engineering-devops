149c480c4d8d2df91a2fcaaa3cdb4ddaae60b6b5
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-07-08 11:09:50 +0100

    edit on CM 1 werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # install Flask version 2.1.0 using pip3
 #
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.3.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
