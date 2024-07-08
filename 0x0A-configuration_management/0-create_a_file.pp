8e68b932f4e617ff8a7ef8d854176dd7c7cfb875
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-07-08 10:41:01 +0100

    edit on lint style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Puppet, create a file in /tmp
-file{'/tmp/school':
-ensure => 'file'.
-mode => '0744',
-content => 'I love Puppet',
-group => 'www-data',
-owner => 'www-data'
-}
+file {'/tmp/school':
+  ensure  => 'file'.
+  mode    => '0744',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  owner   => 'www-data'
+  }
